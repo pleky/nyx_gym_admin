59ea0065abd42f465d708a9f9b15aa0fe9ce9fb8
pleky <onyariantoprapanca@gmail.com>
2026-01-11 15:52:44 +0700

    feat: add owner account seeder with default credentials

---
+++ database/seeders/OwnerSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Nyx Gym Owner',
+            'email' => 'owner@nyxgym.com',
+            'password' => Hash::make('password'),
+            'role' => 'OWNER',
+            'phone' => '+6281111111111',
+            'status' => 'ACTIVE',
+            'email_verified_at' => now(),
+        ]);
+
+        $this->command->info('Owner user created: owner@nyxgym.com');
+        $this->command->info('Password: password');
+        $this->command->info('Role: OWNER');
+        
+    }
+}
